c536a16b782428df2f48ee515a6ef10ab877a12f
yangxu <1135312200@qq.com>
2024-09-10 19:44:27 +0800

    我的班级插件

--- exam_sys/src/pages/manageGroup/groupList/GroupList.tsx
+++ exam_sys/src/pages/manageGroup/groupList/GroupList.tsx
@@ -1,7 +1,7 @@
 
 
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { EllipsisOutlined, PlusOutlined } from '@ant-design/icons';
 import type { ActionType, ProColumns } from '@ant-design/pro-components';
 import { ProTable, TableDropdown } from '@ant-design/pro-components';
@@ -23,6 +23,7 @@ export const waitTime = async (time: number = 100) => {
 };
 
 type GithubIssueItem = {
+  dataIndex:string
   url: string;
   id: number;
   number: number;
@@ -38,132 +39,101 @@ type GithubIssueItem = {
   closed_at?: string;
 };
 
-const columns: ProColumns<GithubIssueItem>[] = [
-  {
-    dataIndex: 'index',
-    valueType: 'indexBorder',
-    width: 48,
-  },
-  {
-    title: '标题',
-    dataIndex: 'title',
-    copyable: true,
-    ellipsis: true,
-    tooltip: '标题过长会自动收缩',
-    formItemProps: {
-      rules: [
-        {
-          required: true,
-          message: '此项为必填项',
-        },
-      ],
-    },
-  },
-  {
-    disable: true,
-    title: '状态',
-    dataIndex: 'state',
-    filters: true,
-    onFilter: true,
-    ellipsis: true,
-    valueType: 'select',
-    valueEnum: {
-      all: { text: '超长'.repeat(50) },
-      open: {
-        text: '未解决',
-        status: 'Error',
-      },
-      closed: {
-        text: '已解决',
-        status: 'Success',
-        disabled: true,
-      },
-      processing: {
-        text: '解决中',
-        status: 'Processing',
-      },
-    },
-  },
-  {
-    disable: true,
-    title: '标签',
-    dataIndex: 'labels',
-    search: false,
-    renderFormItem: (_, { defaultRender }) => {
-      return defaultRender(_);
-    },
-    render: (_, record) => (
-      <Space>
-        {record.labels.map(({ name, color }) => (
-          <Tag color={color} key={name}>
-            {name}
-          </Tag>
-        ))}
-      </Space>
-    ),
-  },
-  {
-    title: '创建时间',
-    key: 'showTime',
-    dataIndex: 'created_at',
-    valueType: 'date',
-    sorter: true,
-    hideInSearch: true,
-  },
-  {
-    title: '创建时间',
-    dataIndex: 'created_at',
-    valueType: 'dateRange',
-    hideInTable: true,
-    search: {
-      transform: (value) => {
-        return {
-          startTime: value[0],
-          endTime: value[1],
-        };
-      },
-    },
-  },
-  {
-    title: '操作',
-    valueType: 'option',
-    key: 'option',
-    render: (text, record, _, action) => [
-      <a
-        key="editable"
-        onClick={() => {
-          action?.startEditable?.(record.id);
-        }}
-      >
-        编辑
-      </a>,
-      <a href={record.url} target="_blank" rel="noopener noreferrer" key="view">
-        查看
-      </a>,
-      <TableDropdown
-        key="actionGroup"
-        onSelect={() => action?.reload()}
-        menus={[
-          { key: 'copy', name: '复制' },
-          { key: 'delete', name: '删除' },
-        ]}
-      />,
-    ],
-  },
-];
+
 const GroupList = () => {
   const actionRef = useRef<ActionType>();
+  const [data, setDate] = useState([])
 
+  // const getListApi = async () => {
+  //   const res = await studentGroupList({page:1,pagesize:5})
+  //   console.log(res.data.data.list)
+  //   // return res.data.data.list
+  //   setDate(res.data.data.list)
+  //   console.log(data)
+  // }
 
-  const getListApi = async () => {
-    const res = await studentGroupList({page:1,pagesize:5})
-    console.log(res.data.data.list)
-    return res.data.data.list
-  }
+  const columns: ProColumns<GithubIssueItem>[] = [
+    {
+      dataIndex: 'index',
+      valueType: 'indexBorder',
+      width: 48,
+    },
+    {
+      title: '班级名称',
+      key: 'showTime',
+      dataIndex: 'classify',
+      // valueType: 'classify',
+      sorter: true,
+      hideInSearch: true,
+    },
+    {
+      title: '班级名称',
+      dataIndex: 'classify',
+      copyable: true,
+      tooltip: '标题过长会自动收缩',
+      formItemProps: {
+        rules: [
+          {
+            required: true,
+            message: '此项为必填项',
+          },
+        ],
+      },
+    },
+   
+    {
+      title: '创建时间',
+      key: 'showTime',
+      dataIndex: 'created_at',
+      valueType: 'date',
+      sorter: true,
+      hideInSearch: true,
+    },
+    {
+      title: '创建时间',
+      dataIndex: 'created_at',
+      valueType: 'dateRange',
+      hideInTable: true,
+      search: {
+        transform: (value) => {
+          return {
+            startTime: value[0],
+            endTime: value[1],
+          };
+        },
+      },
+    },
+    {
+      title: '操作',
+      valueType: 'option',
+      key: 'option',
+      render: (text, record, _, action) => [
+        <a
+          key="editable"
+          onClick={() => {
+            action?.startEditable?.(record.id);
+          }}
+        >
+          编辑
+        </a>,
+        <a href={record.url} target="_blank" rel="noopener noreferrer" key="view">
+          查看
+        </a>,
+        <TableDropdown
+          key="actionGroup"
+          onSelect={() => action?.reload()}
+          menus={[
+            { key: 'copy', name: '复制' },
+            { key: 'delete', name: '删除' },
+          ]}
+        />,
+      ],
+    },
+  ];
 
-  useEffect(()=>{
-    getListApi()
-  },[])
+  // useEffect(()=>{
+  //   getListApi()
+  // },[])
 
   return (
     <>
@@ -172,14 +142,17 @@ const GroupList = () => {
       actionRef={actionRef}
       cardBordered
       request={async (params, sort, filter) => {
-        console.log('1111111111111')
-        console.log(sort, filter);
         await waitTime(2000);
-        return request<{
-          data: GithubIssueItem[];
-        }>('https://proapi.azurewebsites.net/github/issues', {
-          params,
+        const res = await studentGroupList({
+          page: params.current as number,
+          pagesize: params.pageSize as number,
         });
+        console.log('数据', res.data.data.total)
+       return {
+        data: res.data.data.list,
+        success: true,
+        total: res.data.data.total,
+       }
       }}
       
       editable={{
